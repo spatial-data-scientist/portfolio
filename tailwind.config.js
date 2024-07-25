db3601f86eb9b3169b65d82c629074311fc05f30
spatial-data-scientist <elias.haglund@gmail.com>
2024-07-25 19:46:18 +0200

    more themes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './**/*.html',  // Adjust based on your file structure
+    './**/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',  // Adjust if using React or other frameworks
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
